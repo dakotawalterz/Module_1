fb2dc4718795b578b84d2facf3550b3270b70482
dakotawalterz <wal19011@byui.edu>
2023-01-10 13:20:01 -0700

    Initial commit

---
+++ Hello_World.py
@@ -0,0 +1,9 @@
+print("Hello World")
+input("Hello World : Your Response: ")
+
+
+
+
+print('Thank you for responding! Excited for this semester!!!')
+
+#I chose python because it is the programming language I like the most because of it simpleness
---
+++ README.md
@@ -0,0 +1,2 @@
+# Module_1_Hello World
+ This is for people to view a very simple Hello World project
